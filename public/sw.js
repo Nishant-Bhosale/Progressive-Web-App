8811209c9e0f36f0d0c7e260a3bf8296093c1256
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-10-18 12:13:54 +0530

    Precached files with service workers

--- public/sw.js
+++ public/sw.js
@@ -3,7 +3,19 @@ self.addEventListener('install', (event) => {
 	event.waitUntil(
 		caches.open('static').then((cache) => {
 			console.log('[Service Worker] Precaching App Shell');
-			cache.add('/src/js/app.js');
+			cache.addAll([
+				'/',
+				'/index.html',
+				'/src/js/app.js',
+				'/src/js/feed.js',
+				'/src/js/material.min.js',
+				'/src/css/app.css',
+				'/src/css/feed.css',
+				'/src/images/main-image.jpg',
+				'https://fonts.googleapis.com/css?family=Roboto:400,700',
+				'https://cdnjs.cloudflare.com/ajax/libs/material-design-lite/1.3.0/material.indigo-pink.min.css',
+				'https://fonts.googleapis.com/icon?family=Material+Icons',
+			]);
 		}),
 	);
 });
@@ -15,5 +27,17 @@ self.addEventListener('activate', (event) => {
 });
 
 self.addEventListener('fetch', (event) => {
-	event.respondWith(fetch(event.request));
+	caches.keys().then((keys) => {
+		console.log(keys);
+	});
+	event.respondWith(
+		caches.match(event.request).then((response) => {
+			console.log(response);
+			if (response) {
+				return response;
+			} else {
+				return fetch(event.request);
+			}
+		}),
+	);
 });
