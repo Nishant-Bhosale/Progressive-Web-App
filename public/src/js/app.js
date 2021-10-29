388be1e2401130ac6c3f5859a6291d93c7896198
Nishant Bhosale <nishantbhosale244@gmail.com>
2021-10-29 15:08:06 +0530

    Added feature to enable notifications

--- public/src/js/app.js
+++ public/src/js/app.js
@@ -1,20 +1,42 @@
 var deferredPrompt;
 
-if ('serviceWorker' in navigator) {
+const enableNotificationButtons = document.querySelectorAll(
+	".enable-notifications",
+);
+
+if ("serviceWorker" in navigator) {
 	//checks if the browser provides support for service workers
 	navigator.serviceWorker
-		.register('/sw.js') //second param { scope: 'path of sw'} to overwrite scope of the sw
+		.register("/sw.js") //second param { scope: 'path of sw'} to overwrite scope of the sw
 		.then(() => {
-			console.log('Service Worker Registered');
+			console.log("Service Worker Registered");
 		})
 		.catch((e) => {
 			console.log(e);
 		});
 }
 
-window.addEventListener('beforeinstallprompt', (event) => {
-	console.log('beforeinstallprompt fired...');
+window.addEventListener("beforeinstallprompt", (event) => {
+	console.log("beforeinstallprompt fired...");
 	event.preventDefault();
 	deferredPrompt = event;
 	return false;
 });
+
+const grantPermission = () => {
+	console.log("EE");
+	Notification.requestPermission((result) => {
+		if (result !== "granted") {
+			console.log("Notifications not required");
+		} else {
+			console.log("Permission granted");
+		}
+	});
+};
+
+if ("Notification" in window) {
+	for (let i = 0; i < enableNotificationButtons.length; i++) {
+		enableNotificationButtons[i].style.display = "inline-block";
+		enableNotificationButtons[i].addEventListener("click", grantPermission);
+	}
+}
